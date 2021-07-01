50fd06380e698562c4cb6d5c3f4e9bf56696feca
unknown <rajnovicteodora@gmail.com>
2021-07-01 13:05:15 +0200

    Add result write to CSV

--- ORI_Projekat/src/main.py
+++ ORI_Projekat/src/main.py
@@ -1,10 +1,10 @@
-
 import pandas as pandas
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import string
+from sklearn.metrics import mean_squared_error
 
-RECOMMENDATION_NUMBER = 10
+N = 10
 
 
 def clean_string(text):
@@ -14,33 +14,7 @@ def clean_string(text):
     return text
 
 
-def get_recommendations(title, cosine_sim):
-
-    if title not in game_df:
-        return []
-
-    # Get the index of the game that matches the name
-    idx = game_df[title]
-
-    # Get the pairwise similarity scores of all games with that game
-    sim_scores = list(enumerate(cosine_sim[idx]))
-
-    # Sort the games based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Get the scores of the most similar games
-    # (not the first one because this games as a score of 1 (perfect score) similarity with itself)
-    sim_scores = sim_scores[1:RECOMMENDATION_NUMBER + 1]
-
-    # Get the games indices
-    movie_indices = [i[0] for i in sim_scores]
-
-    # Return the top most similar games
-    return game_df['name'].iloc[movie_indices].tolist()
-
-
 if __name__ == '__main__':
-    
     user_df = pandas.read_csv("data/steam-200k.csv")
     game_df = pandas.read_csv("data/steam_games.csv")
     game_df = pandas.DataFrame(game_df)
@@ -50,6 +24,7 @@ if __name__ == '__main__':
 
     # Remove negatively reviewed games
     game_df = game_df[game_df['all_reviews'].str.contains('Negative') == False]
+    game_df = game_df[game_df['all_reviews'].str.contains('Mixed') == False]
 
     # Set hours to 0 if action is purchase
     user_df.loc[(user_df['Action'] == 'purchase') & (user_df['Hours'] == 1.0), 'Hours'] = 0
@@ -60,50 +35,99 @@ if __name__ == '__main__':
 
     # Remove unnecessary columns and duplicate games
     game_df = game_df.iloc[:, 2:-5]
-
     game_df = game_df.drop_duplicates(subset="name", keep='first')\
-        .drop(['achievements', 'desc_snippet', 'recent_reviews', 'achievements', 'languages'], axis=1)
+        .drop(['achievements', 'desc_snippet', 'recent_reviews', 'all_reviews', 'achievements', 'languages'], axis=1)
 
-    # relevant info for recommendation: genre game_details popular_tags publisher developer
-    game_df.loc[:, 'genre'] = game_df['genre'].fillna('')
-    game_df.loc[:, 'game_details'] = game_df['game_details'].fillna('')
-    game_df.loc[:, 'popular_tags'] = game_df['popular_tags'].fillna('')
-    game_df.loc[:, 'publisher'] = game_df['publisher'].fillna('')
-    game_df.loc[:, 'developer'] = game_df['developer'].fillna('')
+    # Features for game comparison
+    features = ['release_date', 'developer', 'publisher', 'popular_tags', 'game_details', 'genre']
+    for feature in features:
+        game_df.loc[:, feature] = game_df[feature].fillna('')
+        #game_df.loc[:, feature] = game_df[feature].apply(clean_string)
 
-    game_df.loc[:, 'genre'] = game_df['genre'].apply(clean_string)
-    game_df.loc[:, 'game_details'] = game_df['game_details'].apply(clean_string)
-    game_df.loc[:, 'popular_tags'] = game_df['popular_tags'].apply(clean_string)
-    game_df.loc[:, 'publisher'] = game_df['publisher'].apply(clean_string)
-    game_df.loc[:, 'developer'] = game_df['developer'].apply(clean_string)
-
-    game_df["genre_publisher_popular_tags_developer_game_details"] = game_df['genre'] + game_df['publisher'] + game_df[
-        'popular_tags'] + game_df['developer'] + game_df['game_details']
+    game_df["release_date_developer_publisher_popular_tags_game_details_genre"] = game_df['release_date'] +\
+        game_df['developer'] + game_df['publisher'] + game_df['popular_tags'] + game_df['game_details'] +\
+        game_df['genre']
 
     # Compute the Cosine Similarity matrix using the column
 
     # CountVectorizer converts strings to numerical vectors
     # stop_words - most frequent words that give no meaning, we remove them
 
-    count = CountVectorizer(stop_words='english')
+    count = CountVectorizer(stop_words='english', lowercase = True, token_pattern = '[a-zA-Z0-9]+')
 
-    count_matrix = count.fit_transform(game_df['genre_publisher_popular_tags_developer_game_details'])
-    cosine_sim_matrix = cosine_similarity(count_matrix, count_matrix)
-
-    #print(cosine_sim_matrix)
-
-    #print(len(user_df))
+    count_matrix = count.fit_transform(game_df['release_date_developer_publisher_popular_tags_game_details_genre'])
+    cosine_similarity = cosine_similarity(count_matrix, count_matrix)
 
     unique_users = user_df.sort_values(['UserID']).drop_duplicates(['UserID'], keep='first')
-    recommendations = pandas.DataFrame(columns=['UserID', 'Games'])
+    recommendations_df = pandas.DataFrame(columns=['UserID', 'Games', 'Recommended Games', 'MSE', 'RMSE', 'Accuracy'])
+
     user_games = list()
+    recommendations = list()
+
+    nesto = list()
+
+    user_games_idx = list()
+    recommendation_game_idx = list()
+
+    top_n_games = list()
     for idx, row in unique_users.iterrows():
-        user_games.append([game for game in user_df[user_df['UserID'] == row['UserID']]['Game']])
+        user_games.append([game for game in user_df[user_df['UserID'] ==  row['UserID']]['Game']])
 
-        # games_for_recommendation = game_df.loc[~game_df['name'].isin(user_games)]
-        # games_for_recommendation = games_for_recommendation.sort_values(by="all_reviews", ascending=False)
-        print(user_games[0])
+        print("========================")
+        print(row['UserID'])
         for game in user_games[0]:
-
             if game in game_df['name'].unique():
                 game_idx = game_df.index[game_df['name'] == game].tolist()[0]
+
+                if game_idx >= len(cosine_similarity):
+                    continue
+
+                # (game, score)
+                similarity = list(enumerate(cosine_similarity[game_idx]))
+
+                user_games_idx.append(game_idx)
+                # Sort by scores
+                similarity = sorted(similarity, key=lambda x: x[1], reverse=True)
+                similarity = similarity[1:2 + 1]
+
+                # Get the games indices
+                #top_n_games = [i[0] for i in similarity]
+                for i in similarity:
+                    top_n_games.append(i)
+
+        top_n_games = sorted(top_n_games, key=lambda x: x[1], reverse=True)
+        for recommended_game in top_n_games:
+            if recommended_game[0] not in recommendation_game_idx:
+                recommendation_game_idx.append(recommended_game[0])
+
+        # Return the top most similar games
+
+        recommendation_game_idx = recommendation_game_idx[:len(user_games_idx)]
+        if len(recommendation_game_idx) == 0 or len(user_games_idx) == 0:
+            continue
+
+        mse = mean_squared_error(user_games_idx, recommendation_game_idx)
+        print(mse)
+
+        rmse = mean_squared_error(user_games_idx, recommendation_game_idx, squared=False)
+        print(rmse)
+        accuracy = len(set(recommendations) & set(user_games[0])) / float(len(user_games[0]))
+        print(accuracy)
+        nesto.append(accuracy)
+        ug = ','.join(user_games[0])
+        gg = ','.join(game_df['name'].iloc[recommendation_game_idx].tolist())
+
+        recommendations_df = recommendations_df.append({'UserID': row['UserID'], 'Games': ug,
+                                                        'Recommended Games': gg,
+                                                        'MSE': mse,
+                                                        'RMSE': rmse,
+                                                        'Accuracy': accuracy}, ignore_index=True)
+        print(recommendations_df)
+        user_games = list()
+        recommendations = list()
+
+        user_games_idx = list()
+        recommendation_game_idx = list()
+        break
+    print(max(nesto))
+    recommendations_df.to_csv("data/recommendation.csv")
