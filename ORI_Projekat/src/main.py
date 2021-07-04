f46697db7f2bb67286d747ceef4e75cb4e5ab818
unknown <rajnovicteodora@gmail.com>
2021-07-04 18:30:45 +0200

    Update main.py

--- ORI_Projekat/src/main.py
+++ ORI_Projekat/src/main.py
@@ -4,8 +4,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
+
 def clean_string(text):
-    return text.replace(" ", "")
+    return text.replace(" ", "").lower()
 
 
 if __name__ == '__main__':
@@ -18,10 +19,10 @@ if __name__ == '__main__':
 
     # Remove negatively reviewed games
     game_df = game_df[game_df['all_reviews'].str.contains('Negative') == False]
-    game_df = game_df[game_df['all_reviews'].str.contains('Mixed') == False]
+    #game_df = game_df[game_df['all_reviews'].str.contains('Mixed') == False]
 
     # Set hours to 1 if action is purchase
-    user_df.loc[(user_df['Action'] == 'purchase') & (user_df['Hours'] == 1.0), 'Hours'] = 1
+    user_df.loc[(user_df['Action'] == 'purchase') & (user_df['Hours'] == 1.0), 'Hours'] = 0.1
 
     # Remove purchased row if game has been played
     user_df = user_df.sort_values(['UserID', 'Game', 'Action']) \
@@ -52,74 +53,106 @@ if __name__ == '__main__':
     cosine_similarity = cosine_similarity(count_matrix, count_matrix)
 
     unique_users = user_df.sort_values(['UserID']).drop_duplicates(['UserID'], keep='first')
-    recommendations_df = pandas.DataFrame(columns=['UserID', 'Games', 'Recommended Games', 'MSE', 'RMSE', 'Accuracy'])
+    unique_games = game_df['name'].unique()
+
+    recommendations_df = pandas.DataFrame(columns=['UserID', 'Games', 'Playtime', 'Recommended Games', 'Predicted', 'MAE', 'RMSE', 'Accuracy'])
     print(len(unique_users))
-    temp = 0
-    for idx, row in unique_users.iterrows():
+    print(len(unique_games))
+
+    # Make random test users
+    random_users = unique_users.sample(150)
+
+    for idx, row in random_users.iterrows():
         user_games = list()
         recommendations = list()
-        user_games_idx = list()
-        top_n_games = list()
 
-        user_games.append([game for game in user_df[user_df['UserID'] == row['UserID']]['Game']])
+        user_games_prediction = list()
+        recommendation_predictions = list()
 
+        user_games.append([game for game in user_df[user_df['UserID'] == row['UserID']]['Game'] if game in unique_games])
+        if len(user_games[0]) <= 1:
+            continue
         for game in user_games[0]:
-            if game in game_df['name'].unique():
-                game_idx = game_df.index[game_df['name'] == game].tolist()[0]
+            top_n_games = list()
+            game_idx = game_df.index[game_df['name'] == game].tolist()[0]
 
-                if game_idx >= len(cosine_similarity):
-                    continue
+            if game_idx >= len(cosine_similarity):
+                continue
 
-                # (game, score)
-                similarity = list(enumerate(cosine_similarity[game_idx]))
+            # (game, score)
+            similarity = list(enumerate(cosine_similarity[game_idx]))
 
-                user_games_idx.append(game_idx)
+            # Sort by scores
+            similarity = sorted(similarity, key=lambda x: x[1], reverse=True)
 
-                # Sort by scores
-                similarity = sorted(similarity, key=lambda x: x[1], reverse=True)
+            # Index of current game that we are predicting for
+            user_games_prediction.append(user_df.loc[(user_df['UserID'] == row['UserID']) & (user_df['Game'] == game)]['Hours'].values[0])
 
-                # Set of similar games
-                similarity = similarity[1:2 + 1]
+            # Top k similar games
+            similar_games = similarity[1:20 + 1]
 
+            # Make predictions
+            # For similar games
+            for i in similar_games:
 
-                # Sum of all similar games to current game
-                denominator = sum([abs(x[1]) for x in similarity])
+                denominator = 0.0
                 num = 0.0
-                for i in similarity:
-                    temp = game_df['name'].iloc[i[0]].tolist()
-                    num += i[1] * user_df[user_df['UserID'] == row['UserID'] and user_df['Game'] == temp]
+                # i - current game that we are calculating hours for
+
+                # For each game that user has played
+                for rated_game in user_games[0]:
+                    sim_game_idx = game_df.index[game_df['name'] == rated_game].tolist()[0]
+
+                    if sim_game_idx >= len(cosine_similarity) or rated_game == game_idx:
+                        continue
+
+                    # Sim of current game and previously rated game
+                    sim = cosine_similarity[i[0]][sim_game_idx]
+
+                    # Hours of game playtime
+                    hours_played = user_df.loc[(user_df['UserID'] == row['UserID']) & (user_df['Game'] == rated_game)]['Hours'].values[0]
+
+                    num += hours_played * sim
+                    denominator += abs(sim)
 
                 if denominator > 0.0:
-                    result = num/denominator\
-                    top_n_games.append((i[0], reusult)
-        top_n_games = sorted(top_n_games, key=lambda x: x[1], reverse=True)
-        for recommended_game in top_n_games:
-            if recommended_game[0] not in recommendations:
-                recommendations.append(recommended_game[0])
+                    result = num/denominator
+                    top_n_games.append((i[0], result))
+                else:
+                    top_n_games.append((i[0], 0))
 
-        # Return the top most similar games
-        recommendations = recommendations[:len(user_games_idx)]
+            # Pick the most similar game
+            top_n_games = sorted(top_n_games, key=lambda x: x[1], reverse=True)
+
+            for recommended_game in top_n_games:
+                if recommended_game[0] not in recommendations:
+                    recommendations.append(recommended_game[0])
+                    recommendation_predictions.append(recommended_game[1])
+                    break
 
         # If recommendations aren't found
-        if len(recommendations) == 0 or len(user_games_idx) == 0:
+        if len(recommendations) == 0 or len(user_games) == 0:
             continue
 
-        mse = mean_squared_error(user_games_idx, recommendations)
-        rmse = mean_squared_error(user_games_idx, recommendations, squared=False)
+        mae = mean_absolute_error(user_games_prediction, recommendation_predictions)
+        rmse = mean_squared_error(user_games_prediction, recommendation_predictions, squared=False)
         recommendations = game_df['name'].iloc[recommendations].tolist()
         accuracy = len(set(recommendations) & set(user_games[0])) / float(len(user_games[0]))
 
-        temp += 1
+        print(accuracy)
 
-
+        user_games_prediction = [str(a) for a in user_games_prediction]
+        recommendation_predictions = [str(a) for a in recommendation_predictions]
         user_games_print = ','.join(user_games[0])
         recommendation_print = ','.join(recommendations)
-
+        playtime = ','.join(user_games_prediction)
+        predicted = ','.join(recommendation_predictions)
         recommendations_df = recommendations_df.append({'UserID': row['UserID'], 'Games': user_games_print,
+                                                        'Playtime': playtime,
                                                         'Recommended Games': recommendation_print,
-                                                        'MSE': mse,
+                                                        'Predicted': predicted,
+                                                        'MAE': mae,
                                                         'RMSE': rmse,
                                                         'Accuracy': accuracy}, ignore_index=True)
 
-    print(temp)
     recommendations_df.to_csv("data/recommendation.csv")
